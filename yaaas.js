f08e283f3533f97399b6f0863a02e14923b99bee
beartums <eric@griffithnet.com>
2014-06-19 22:27:01 -0400

    initial commit

---
+++ yaaas.js
@@ -0,0 +1,55 @@
+var yaaasApp = angular.module('yaaasApp', []);
+
+yaaasApp.service('yaaas',function($timeout, $rootScope) {
+  var Alert = function(title,text,timeout,alertLevel) {
+	  this.title = title || '';
+	  this.text = text || '';
+	  this.timeout = timeout || 5;
+	  this.alertLevel = alertLevel || 'info';
+	  this.timestamp = new Date();
+  };
+  
+  Alert.prototype.isLevel = function(level) {
+	  return this.alertLevel==level;
+  };
+  
+  var yaaas = {};
+  
+  var _alerts = [];
+  var _alertsHistory = [];
+  
+  yaaas.removeAlert = function(alert) {
+	  var i = _alerts.indexOf(alert);
+	  _alerts.splice(i,1);
+	  $rootScope.yaaasAlerts = _alerts;
+	  $rootScope.$digest();
+  };
+  
+  yaaas.addAlert = function(title, text, timeout, alertLevel) {
+	  var alert = new Alert(title,text,timeout,alertLevel);
+	  _alertsHistory.push(alert);
+	  _alerts.push(alert);
+	  if (timeout>0) {
+		  $timeout(function() {
+			  yaaas.removeAlert(alert);
+		  }, alert.timeout * 1000);
+	  }
+	  $rootScope.yaaasAlerts=_alerts;
+	  //$rootScope.$digest();
+  };
+  
+  yaaas.getAlertsHistory = function() {
+	  return _alertsHistory;
+  };
+  
+  
+  return yaaas;
+});
+
+yaaasApp.directive('yaaAlert', function() {
+	return {
+		restrict: 'EA',
+		templateUrl: 'yaaas.tpl.html'
+	};
+});
+	
