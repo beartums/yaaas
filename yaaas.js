38ab6d1e15799b3985d1499d6740bfd1357edceb
beartums <'eric@griffithnet.com'>
2014-06-23 20:39:11 -0400

    Give attributes more control over positioning, allowing specific pixel counts from specified edge

--- yaaas.js
+++ yaaas.js
@@ -1,10 +1,8 @@
 var yaaasApp = angular.module('yaaas', []);
 
 yaaasApp.constant('template',
-		"<div class='yaaas-alerts {{ isPe ? \"yaaas-pe\" : \"yaaas-not-pe\" }} " +
-				"{{vPos ?  \"yaaas-\" + vPos :  \"yaaas-top\"}} " +
-				"{{hPos ?  \"yaaas-\" + hPos :  \"yaaas-right\"}}' \n" +
-		"		style='width: {{width || \"300px\"}}'>\n" +
+		"<div class='yaaas-alerts {{ isPe ? \"yaaas-pe\" : \"yaaas-not-pe\" }}' \n" +
+		"		style='{{getPosStyle(vPos,\"v\")}}{{getPosStyle(hPos,\"h\")}}{{getWidthStyle()}}'>\n" +
 		"	<div ng-repeat='alert in yaaas.yaaasAlerts' \n" +
 		"			class='alert yaaas-alert {{ isPe ? \"yaaas-pe\" : \"yaaas-not-pe\" }}' \n" +
 		"			ng-class=\"{'alert-info':alert.isLevel('info'),\n" +
@@ -67,25 +65,73 @@ yaaasApp.service('yaaaService',function($timeout) {
   return yaaas;
 });
 
-yaaasApp.directive('yaaAlert', function(template, yaaaService) {
+yaaasApp.directive('yaaAlert', function(template, yaaaService, $window) {
+	
 	return {
 		restrict: 'EA',
 		scope: {
 			vPos: "@vPos",
 			hPos: "@hPos",
-			isPe: "@pe",
+			pe: "@pe",
 			width: "@width"
-			},
+		},
 		template: template,
-		compile: function(element,attrs) {
+		link: function(scope,element,attrs) {
 			
+			scope.yaaas = yaaaService;
 			
-			return function(scope,element,attrs) {
-				var b = element.html();
-				scope.yaaas = yaaaService;
-				var c = b;
+			scope.isPe = function() {
+				pe = pe ? pe.toLowerCase()=='true': false;
 			};
+			
+			scope.getWidthStyle = function(width) {
+				width = width || scope.width || '300';
+				width = width.replace(/\D/g,'');
+				return 'width: ' + width + 'px; ';				
+			};
+			
+			/**
+			 * @function
+			 * @description Return the position parameters for a specified 
+			 * position attribute
+			 * @param {string} posType 'V' for vertical position attribute, 'H' for horizontal
+			 * @param {string} posData Left/Right (for 'H'), 'Top/Bottom' (for 'V')
+			 * OR, if numeric, + number indicates ofset from top or left 
+			 * and - number indicates offset from right or bottom based on posType;
+			 * 0 will be considered negative for defaulting position
+			 */
+			scope.getPosStyle = function(posData, posType) {
+				posData = posData ? posData.toLowerCase() : '';
+				posType = posType ? posType.toLowerCase() : '';
+				if (posType != 'v' && posType != 'h') return '';
+				
+				var posDefaults = posType == 'v' ? 
+						{top: 0, bottom: 0} : {right: -20, left: 20};
+				var posValues = posType == 'v' ?
+						['top','bottom'] : ['left','right']
+				
+				if (isNaN(parseInt(posData))) {	
+					posData = (posValues.indexOf(posData)>-1) ? posData : '';
+					if (!posData && posType) {
+						posData = posType == 'v' ? 'top' : 'right'
+					}
+					var pos = posDefaults[posData] ? posDefaults[posData] : 0;
+				} else {
+					var pos = parseInt(posData);
+					if (pos<=0) {
+						posData = posType == 'v' ? 'bottom' : 'right';
+						pos = -1 * pos
+					} else {
+						posData = posType == 'v' ? 'top' : 'left';
+					}
+				}
+				
+				return posData + ': ' + pos + 'px; '
+			};
+			
+	
 		}
+		
 		
 	};
 });
